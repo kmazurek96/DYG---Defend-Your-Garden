ddf49e744e441f7b892bf3bc42367f35f5fa8849
kmazurek96 <konradmazurek12@gmail.com>
2021-06-24 14:09:18 +0200

    DefenderShooter script
    If attacker is on line, attack.
    Corrected Rb2 in lizard prefab

--- Assets/Scripts/DefenderShooter.cs
+++ Assets/Scripts/DefenderShooter.cs
@@ -5,11 +5,54 @@ using UnityEngine;
 public class DefenderShooter : MonoBehaviour
 {
     [SerializeField]GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if(AttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+
+    void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
         
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool isInLine = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if(isInLine)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    bool AttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
 
     void Shot()
